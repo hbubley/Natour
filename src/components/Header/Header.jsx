b640498702ca59adcbd892b5117e67a1d2fdd619
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-28 12:22:48 -0500

    creating header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Header.module.css'
+import logo from '../../img/logo-white.png'
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logoBox}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </div>
+      <div className={styles.textBox}>
+        <h1 className={styles.headingPrimary}>
+          <span className={styles.headingPrimaryMain}>Outdoors</span>
+          <span className={styles.headingPrimarySub}>is where life happens</span>
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
