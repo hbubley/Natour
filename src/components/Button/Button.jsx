288fe38838d52611e6f9b8a66e5dc6b667299f17
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-28 19:57:20 -0500

    small fixes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ text, variant, animated, text, onClick }) => {
+const Button = ({ text, variant, animated, text, onClick, link }) => {
   const _animated = animated ? styles.buttonAnimated : ''
   const _variant = primary ? styles.buttonWhite: ''
   return (
