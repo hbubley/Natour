53c0030385372a64e25b87f661fab5a22ef2c133
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-29 23:17:16 -0500

    hard to fully commit to this file structure

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './About.module.scss';
+
+const About = () => {
+    return (
+        <div className={styles.sectionAbout}>
+            <h2>Exciting tours for adventures people</h2>
+        </div>
+    )
+}
+
+export default About
