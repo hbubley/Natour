1c618adae24376a81d0b90d2c6db000df7c0338b
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-30 10:57:36 -0500

    begin organizing things into files

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import styles from './About.module.scss';
 const About = () => {
     return (
         <div className={styles.sectionAbout}>
-            <h2>Exciting tours for adventures people</h2>
+            <h2 className={styles.headingSecondary}>Exciting tours for adventures people</h2>
         </div>
     )
 }
