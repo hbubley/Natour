0c4fdc424ec033f92865bb4899b46073a91335a5
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-30 13:22:59 -0500

    add another button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 <div className={`${styles.col} ${styles.col1Of2}`}>
                     <TertiaryTextBlock heading={"You're going to fall in love with nature"} paragraph={"Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus."} />
                     <TertiaryTextBlock heading={"Live adventures like you never have before"} paragraph={"Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Vivamus suscipit tortor eget felis porttitor volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
-                    <a href="#" class="buttonText">Learn More</a>
+                    <a href="#" className={styles.buttonText}>Learn More</a>
                 </div>
                 <div className={`${styles.col} ${styles.col1Of2}`}>
                     Image Composition
