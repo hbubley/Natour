0447e5b1421c73d6bf88bb8076f50705f4b48e3d
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-03-31 12:04:45 -0500

    begin features work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About/About'
+import Features from './components/Features/Features'
 import Header from './components/Header/Header'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <About />
+      <Features />
     </div>
   )
 }
---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Features.module.scss';
+import icons from '../../utilities/icons/icon-font.css'
+const Features = () => {
+  return (
+    <div className={styles.sectionFeatures}>
+        <i className="icon-basic-world" />
+    </div>
+  )
+}
+
+export default Features
