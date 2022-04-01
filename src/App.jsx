bdad06b5abf42cedb534b80ee0330053284412b5
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-04-01 08:47:54 -0500

    get rteady to begin work on tours section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About/About'
 import Features from './components/Features/Features'
 import Header from './components/Header/Header'
+import Tours from './components/Tours/Tours'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Features />
+      <Tours />
     </div>
   )
 }
---
+++ src/components/Tours/Tours.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Tours.module.scss'
+const Tours = () => {
+  return (
+    <div className={styles.sectionTours}>Tours</div>
+  )
+}
+
+export default Tours
