8e81894968d5b4141659f56d5b02597065d8d72f
Haley Bubley <haleybubley@Haleys-iMac.lan>
2022-04-02 15:00:48 -0500

    Added styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import styles from './Card.module.scss'
-const Card = ({variant}) => {
+import variables from '../../utilities/_variables.scss'
+
+const Card = ({ variant, image, heading, details }) => {
+    const backgroundImagePrimaryStyles = { backgroundImage: `url(${image}), linear-gradient(to right bottom, #ffb900, #ff7730)`, backgroundBlendMode: 'screen' }
+    const backgroundImageSecondaryStyles = { backgroundImage: `url(${image}), linear-gradient(to right bottom, #7ed56f, #28b485)`, backgroundBlendMode: 'screen' }
+    const backgroundImageTertiaryStyles = { backgroundImage: `url(${image}), linear-gradient(to right bottom, #2998ff, #5643fa)`, backgroundBlendMode: 'screen' }
+
     return (
         <div className={styles.card}>
             <div className={styles.cardSideFront}>
-                Front
+                <div
+                    className={`${styles.cardPicture} ${variant === "primary" && styles.cardBackgroundColorPrimary} ${variant === "secondary" && styles.cardBackgroundColorSecondary} ${variant === "tertiary" && styles.cardBackgroundColorTertiary}`}
+                    style={variant === "primary" ? backgroundImagePrimaryStyles: variant === "secondary" ? backgroundImageSecondaryStyles: backgroundImageTertiaryStyles}
+                >
+                </div>
+                <div className={styles.cardHeading}>
+                    {heading}
+                </div>
+                <div className={styles.cardDetails}>
+                    {details}
+                </div>
             </div>
             <div className={`${styles.cardSideBack} ${variant === "primary" && styles.cardBackgroundColorPrimary} ${variant === "secondary" && styles.cardBackgroundColorSecondary} ${variant === "tertiary" && styles.cardBackgroundColorTertiary}`}>
                 Back
--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../Card/Card'
 import SecondaryHeading from '../SecondaryHeading/SecondaryHeading'
 import styles from './Tours.module.scss'
+import backgroundImage1 from '../../img/nat-5.jpg'
 const Tours = () => {
   return (
     <div className={styles.sectionTours}>
@@ -10,13 +11,13 @@ const Tours = () => {
       </div>
       <div className={styles.row}>
         <div className={`${styles.col} ${styles.col1Of3}`}>
-          <Card variant={"primary"} />
+          <Card variant={"primary"} heading={"The sea explorer"} details={"Test"} image={backgroundImage1} />
         </div>
         <div className={`${styles.col} ${styles.col1Of3}`}>
-          <Card variant={"secondary"} />
+          <Card variant={"secondary"} heading={"Test"} details={"Test"} image={backgroundImage1} />
         </div>
         <div className={`${styles.col} ${styles.col1Of3}`}>
-          <Card variant={"tertiary"} />
+          <Card variant={"tertiary"} heading={"Test"} details={"Test"} image={backgroundImage1} />
         </div>
       </div>
     </div>
