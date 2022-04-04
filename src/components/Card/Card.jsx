04d3ac2dbbb9f428e8d3436fe898bcf8c4d56720
Haley Bubley <haleybubley@haleys-imac.lan>
2022-04-04 10:19:46 -0500

    added front-card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import styles from './Card.module.scss'
 import variables from '../../utilities/_variables.scss'
 
-const Card = ({ variant, image, heading, details }) => {
+const Card = ({ variant, image, headingLine1, headingLine2, details }) => {
     const backgroundImagePrimaryStyles = { backgroundImage: `url(${image}), linear-gradient(to right bottom, #ffb900, #ff7730)`, backgroundBlendMode: 'screen' }
     const backgroundImageSecondaryStyles = { backgroundImage: `url(${image}), linear-gradient(to right bottom, #7ed56f, #28b485)`, backgroundBlendMode: 'screen' }
     const backgroundImageTertiaryStyles = { backgroundImage: `url(${image}), linear-gradient(to right bottom, #2998ff, #5643fa)`, backgroundBlendMode: 'screen' }
-
+    console.log(styles.cardDetails)
     return (
         <div className={styles.card}>
             <div className={styles.cardSideFront}>
                 <div
-                    className={`${styles.cardPicture} ${variant === "primary" && styles.cardBackgroundColorPrimary} ${variant === "secondary" && styles.cardBackgroundColorSecondary} ${variant === "tertiary" && styles.cardBackgroundColorTertiary}`}
-                    style={variant === "primary" ? backgroundImagePrimaryStyles: variant === "secondary" ? backgroundImageSecondaryStyles: backgroundImageTertiaryStyles}
+                    className={styles.cardPicture}
+                    style={variant === "primary" ? backgroundImagePrimaryStyles : variant === "secondary" ? backgroundImageSecondaryStyles : backgroundImageTertiaryStyles}
                 >
                 </div>
                 <div className={styles.cardHeading}>
-                    {heading}
+                    <span className={`${styles.cardHeadingSpan1} ${styles.cardHeadingSpan} ${variant === "primary" && styles.cardBackgroundColorPrimary} ${variant === "secondary" && styles.cardBackgroundColorSecondary} ${variant === "tertiary" && styles.cardBackgroundColorTertiary}`}>{headingLine1}</span>
+                    <span className={`${styles.cardHeadingSpan2} ${styles.cardHeadingSpan} ${variant === "primary" && styles.cardBackgroundColorPrimary} ${variant === "secondary" && styles.cardBackgroundColorSecondary} ${variant === "tertiary" && styles.cardBackgroundColorTertiary}`}>{headingLine2}</span>
                 </div>
                 <div className={styles.cardDetails}>
-                    {details}
+                    <ul>
+                        {details.map(detail => {
+                            return <li>{detail}</li>
+                        })}
+                    </ul>
                 </div>
             </div>
             <div className={`${styles.cardSideBack} ${variant === "primary" && styles.cardBackgroundColorPrimary} ${variant === "secondary" && styles.cardBackgroundColorSecondary} ${variant === "tertiary" && styles.cardBackgroundColorTertiary}`}>
--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -4,21 +4,42 @@ import SecondaryHeading from '../SecondaryHeading/SecondaryHeading'
 import styles from './Tours.module.scss'
 import backgroundImage1 from '../../img/nat-5.jpg'
 const Tours = () => {
+  const toursData = [
+    {
+      variant: 'primary',
+      headingLine1: "The sea",
+      headingLine2: "explorer",
+      details: ["3 day tours", "up to 30 people", "2 hour guides", "sleep in cozy hotels", "difficulty: easy"],
+      image: backgroundImage1,
+    },
+    {
+      variant: 'secondary',
+      headingLine1: "The sea",
+      headingLine2: "explorer",
+      details: ["3 day tours", "up to 30 people", "2 hour guides", "sleep in cozy hotels", "difficulty: easy"],
+      image: backgroundImage1,
+    },
+    {
+      variant: 'tertiary',
+      headingLine1: "The sea",
+      headingLine2: "explorer",
+      details: ["3 day tours", "up to 30 people", "2 hour guides", "sleep in cozy hotels", "difficulty: easy"],
+      image: backgroundImage1,
+    },
+  ]
   return (
     <div className={styles.sectionTours}>
       <div className={`${styles.centerText} ${styles.marginBottomLarge}`}>
         <SecondaryHeading text={"Most Popular Tours"} />
       </div>
       <div className={styles.row}>
-        <div className={`${styles.col} ${styles.col1Of3}`}>
-          <Card variant={"primary"} heading={"The sea explorer"} details={"Test"} image={backgroundImage1} />
-        </div>
-        <div className={`${styles.col} ${styles.col1Of3}`}>
-          <Card variant={"secondary"} heading={"Test"} details={"Test"} image={backgroundImage1} />
-        </div>
-        <div className={`${styles.col} ${styles.col1Of3}`}>
-          <Card variant={"tertiary"} heading={"Test"} details={"Test"} image={backgroundImage1} />
-        </div>
+        {toursData.map((data) => {
+          return (
+            <div className={`${styles.col} ${styles.col1Of3}`}>
+              <Card variant={data.variant} headingLine1={data.headingLine1} headingLine2={data.headingLine2} details={data.details} image={data.image} />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
